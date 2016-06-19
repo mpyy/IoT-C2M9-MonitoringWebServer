4cdfb9d7c669df66fcafc3e362cd984160cf4c7f
Matias Pyy <matias.pyy@gmail.com>
2016-06-19 19:51:36 +0300

    Add new package: db410c_gpiolib. Implement WebServerService class.

--- app/src/main/java/com/example/matias/c2m9monitoringwebserver/WebServerService.java
+++ app/src/main/java/com/example/matias/c2m9monitoringwebserver/WebServerService.java
@@ -6,11 +6,22 @@ import android.app.Service;
 
 public class WebServerService extends Service {
 
-  public void onCreate() {
-  }
+    /**
+     * Creates an instance of WebServer and runs it in the background
+     */
+    @Override
+    public void onCreate() {
+        new WebServer(this);
+    }
 
-  public IBinder onBind(Intent intent) {
-  return null;
-  }
+    /**
+     * Don't provide any means of getting a persistent connection to the service
+     *
+     * @param intent
+     * @return Always null
+     */
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
 }
---
+++ app/src/main/java/com/example/matias/c2m9monitoringwebserver/db410c_gpiolib/GpioProcessor.java
@@ -0,0 +1,7 @@
+package com.example.matias.c2m9monitoringwebserver.db410c_gpiolib;
+
+/**
+ * Created by matias on 19.6.2016.
+ */
+public class GpioProcessor {
+}
